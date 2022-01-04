1f099993fc01ed1d110c077e224159ea15abf3a2
Glitch (glitch-hello-website) <none>
2022-01-04 13:17:14 +0000

    👑🤑 Checkpoint ./script.js:39999491/1986 ./index.html:39999491/43

--- script.js
+++ script.js
@@ -1,18 +1,35 @@
-console.log("Hello, I'm MindX")
-console.log("Nice to see you!")
-console.log(20)
+console.log("Hello, I'm MindX");
+console.log("Nice to see you!");
+console.log(20);
 
 /*
 Biến là 1 khái niệm cơ bản trong JS
 Biến được sử dụng để lưu trữ giá trị và tái sử dụng sau này
 */
 //let là từ khoá để khai báo 1 biến trong JS
-let age = 18
-console.log(age)
-age = 28
-console.log(age)
-console.log(age+1)
+let age = 18;
+console.log(age);
+age = 28;
+console.log(age);
+console.log(age + 1);
 
-let address ="Hanoi"
-address = "Saigon"
-console.log(age, address)
+let address = "Hanoi";
+address = "Saigon";
+console.log(age, address);
+
+//const được sử dụng để khai báo hằng số
+//Hằng số là một biến không thể thay đổi giá trị
+const myname = "Phong";
+const g = 9.8;
+const pi = 3.14;
+
+//Toán tử toán học
+console.log(1 + 1);
+console.log(1 - 1);
+console.log(2 * 2);
+console.log(4 / 2);
+console.log(5 % 3);
+let sum = 1 + 1;
+console.log(sum / 2);
+sum = sum + 1
+sum
