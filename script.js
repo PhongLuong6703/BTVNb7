e187fbfa74bf429901382ccdc705725d72841b1b
Glitch (glitch-hello-website) <none>
2022-01-04 13:06:03 +0000

    ☕️📰 Checkpoint ./script.js:39999491/550

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 console.log("Hello, I'm MindX")
 console.log("Nice to see you!")
 console.log(20)
+
+/*
+Biến là 1 khái niệm cơ bản trong JS
+Biến được sử dụng để lưu trữ giá trị và tái sử dụng sau này
+*/
+//let là từ khoá để khai báo 1 biến trong JS
+let age = 18
+console.log(age)
+age = 28
+console.log(age)
+console.log(age+1)
+
+let address ="Hanoi"
+address = "Saigon"
+console.log(age, address)
